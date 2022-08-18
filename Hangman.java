0bc88a265644025b2c24292ebcf537d22802d88f
S <s>
2022-08-18 15:06:47 +1200

    {"types":"A","runs":0}

---
+++ Hangman.java
@@ -0,0 +1,33 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class Hangman extends ConsoleProgram {
+
+    public void run() {
+    	setupGame();
+		playGame();
+	}
+    private void setupGame(){
+    	println("Welcome to Hangman!");
+    	
+    	
+    }
+    private void playGame(){
+    	
+    }
+    
+    private String chooseRandomWord(){
+    	return "String";
+    }
+}
+	
