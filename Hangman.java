48c66f3bcc98bf30c09e2d9cd8698e732bdf1fb6
S <s>
2022-08-19 13:56:45 +1200

    {"types":"C","runs":0}

--- Hangman.java
+++ Hangman.java
@@ -15,7 +15,7 @@ public class Hangman extends ConsoleProgram {
 // 	instance variables
 	HangmanLexicon hangmanWords = new HangmanLexicon();
 	// Random number generator
-	private RandomGenerator rgen = RandomGenerator.nextInt();
+	private RandomGenerator rgen = RandomGenerator.nextInt(10);
 	
     public void run() {
     	setupGame();
