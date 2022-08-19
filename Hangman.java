6beb5115ca435e886018a166e0edf17e66663ede
S <s>
2022-08-19 14:06:38 +1200

    {"types":"C","runs":0}

--- Hangman.java
+++ Hangman.java
@@ -12,27 +12,34 @@ import acm.util.*;
 import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
-// 	instance variables
+	// 	instance variables
 	HangmanLexicon hangmanWords = new HangmanLexicon();
 	// Random number generator
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
-    public void run() {
-    	setupGame();
+
+	public void run() {
+		setupGame();
 		playGame();
 	}
-    private void setupGame(){
-    	String secretWord = hangmanWords.getWord(rgen.nextInt(10));
-    	println("Welcome to Hangman!");
-    	println("The word looks like this")
-    	
-    }
-    private void playGame(){
-    	
-    }
-    
-    private String chooseRandomWord(){
-    	return "String";
-    }
+	private void setupGame(){
+		// Getting the secret word from HangmanLexicon through hangmanWords
+		String secretWord = hangmanWords.getWord(rgen.nextInt(10));
+		// making the hint
+		String wordState;
+		for (int i = 0; i < secretWord.length(); i++) {
+			
+			
+		}
+		// Introducing the game
+		println("Welcome to Hangman!");
+		println("The word looks like this: ");
+
+	}
+	private void playGame(){
+
+	}
+
+	private String chooseRandomWord(){
+		return "String";
+	}
 }
-	
