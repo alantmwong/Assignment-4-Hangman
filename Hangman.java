7b87c74921c5d74850fe443573f67622943a925e
S <s>
2022-08-24 13:50:36 +1200

    {"types":"C","runs":8}

--- Hangman.java
+++ Hangman.java
@@ -50,28 +50,32 @@ public class Hangman extends ConsoleProgram {
 		while (guessesLeft != 0 || wordState != secretWord) {
 			println("The word now looks like this: " + wordState);
 			// get guess from the user
-			String guess = readLine("Your guess: ");
-			if (guess.length() == 1) { // making sure the guess is a single character
-				// converting string to char
-				char guessChar = guess.charAt(0);
-				if (Character.isLetter(guessChar)) { // making sure guess is a letter
-					Character.toUpperCase(guessChar);
-					// looping through secretWord to find matching letters to the guess
-					for (int i = 0; i < secretWord.length(); i++) {
-						if (Character.toUpperCase(guessChar) == 
-							Character.toUpperCase(secretWord.charAt(i)) && i != 0) {
-							wordState = wordState.substring(0, i) + guess + wordState.substring(i+1);
-						} else if (Character.toUpperCase(guessChar) == 
-							Character.toUpperCase(secretWord.charAt(i)) && i == 0) {
-							wordState = guess + wordState.substring(i+1);
-						}
+			guess = readLine("Your guess: ");
+			
+		}
+	}
+	
+	private boolean checkGuess() {
+		if (guess.length() == 1) { // making sure the guess is a single character
+			// converting string to char
+			char guessChar = guess.charAt(0);
+			if (Character.isLetter(guessChar)) { // making sure guess is a letter
+				Character.toUpperCase(guessChar);
+				// looping through secretWord to find matching letters to the guess
+				for (int i = 0; i < secretWord.length(); i++) {
+					if (Character.toUpperCase(guessChar) == 
+						Character.toUpperCase(secretWord.charAt(i)) && i != 0) {
+						wordState = wordState.substring(0, i) + guess + wordState.substring(i+1);
+					} else if (Character.toUpperCase(guessChar) == 
+						Character.toUpperCase(secretWord.charAt(i)) && i == 0) {
+						wordState = guess + wordState.substring(i+1);
 					}
-				} else {
-					println("Error - guess was not a letter.");
 				}
 			} else {
-				println("Error - guess was not a single letter.");
+				println("Error - guess was not a letter.");
 			}
+		} else {
+			println("Error - guess was not a single letter.");
 		}
 	}
 }
